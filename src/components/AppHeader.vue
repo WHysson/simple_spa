f73851b94f290b46c0ac74e242f85fccbeaab223
Oleg <kosanostra0696@gmail.com>
2020-01-23 10:03:50 +0200

    customizing toolbar

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,68 @@
+<template>
+  <v-app>
+      <v-navigation-drawer temporary v-model="drawer" class="hidden-md-and-up">
+          <v-list v-for="item in menuItems" :key="item.title" >
+              <v-list-item>{{item.title}}</v-list-item>
+          </v-list>
+      </v-navigation-drawer>
+      <v-app-bar app dark class="primary">
+          <v-app-bar-nav-icon @click.stop="drawer = !drawer" class="hidden-md-and-up"></v-app-bar-nav-icon>
+          <v-toolbar-title>A CUT OF ART</v-toolbar-title>
+          <v-spacer></v-spacer>
+          <v-item-group v-for="item in menuItems" :key="item.title" class="hidden-sm-and-down">
+              <v-btn :to="item.route" text><v-icon left v-html="item.icon"></v-icon>{{item.title}}</v-btn>
+          </v-item-group>
+      </v-app-bar>
+  </v-app>
+</template>
+
+<script>
+export default {
+    data(){
+            return {
+                drawer:false
+            }
+        },
+    computed:{
+        menuItems(){
+            return [
+                {
+                    title: 'Home',
+                    route: '/home',
+                    icon: 'home'
+                },
+                {
+                    title: 'Services',
+                    route: '/services',
+                    icon: 'list'
+                },
+                {
+                    title: 'Profile',
+                    route: '/profile',
+                    icon: 'badge'
+                },
+                {
+                    title: 'Log out',
+                    route: '/logout',
+                    icon: 'exit_to_app'
+                },
+                {
+                    title: 'Sign in',
+                    route: '/signin',
+                    icon: 'input'
+                },
+                {
+                    title: 'Sign up',
+                    route: '/signup',
+                    icon: 'lock_open'
+                },
+            ]
+        }
+    }
+
+}
+</script>
+
+<style scoped>
+
+</style>
--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -4,4 +4,17 @@ import Vuetify from 'vuetify/lib';
 Vue.use(Vuetify);
 
 export default new Vuetify({
+    theme: {
+        themes: {
+          light: {
+            primary: '#212121',
+            secondary: '#607d8b',
+            accent: '#2196f3',
+            error: '#e91e63',
+            warning: '#ffc107',
+            info: '#00bcd4',
+            success: '#4caf50'
+          },
+        },
+      },
 });
